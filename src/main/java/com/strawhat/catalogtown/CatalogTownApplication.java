e52ab27d468f4db9496111789f71700832d9b335
Pratyush Choudhary <solixious@gmail.com>
2021-10-21 09:14:15 +0530

    Initial commit

---
+++ src/main/java/com/strawhat/catalogtown/CatalogTownApplication.java
@@ -0,0 +1,13 @@
+package com.strawhat.catalogtown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strawhat/catalogtown/CatalogTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strawhat.catalogtown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
